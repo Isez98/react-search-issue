7c12adf5da444bc0338648c7b34dd09c2b5592ce
Isacc Hernandez <isacc1798@gmail.com>
2020-07-17 12:46:39 -0700

    Refined issue search

--- src/Components/SearchResults/index.js
+++ src/Components/SearchResults/index.js
@@ -5,11 +5,14 @@ import '../../App/App.css'
 class SearchResults extends React.Component{
   constructor(props) {
     super(props);
-    this.state = { searchResults: [], inputValue: '' }
+    this.state = { searchResults: [], inputValue: '', cursor: 0, gray: true }
     this.handleChange = this.handleChange.bind(this)
+    this.handleKeyDown = this.handleKeyDown.bind(this)
   }
 
- async handleChange(e){
+  async handleChange(e){
+    let cursor = 1;
+    this.setState({cursor});
     this.setState({ inputValue: e.target.value })
     let searchResults = []
     if(e.target.value !== "" )
@@ -17,20 +20,49 @@ class SearchResults extends React.Component{
     this.setState({ searchResults });
   }
 
+  handleKeyDown(e) {
+    const cursor = this.state.cursor;
+    const searchResults = this.state.searchResults;
+    if (e.keyCode === 38 && cursor >= 0) {
+      this.setState( prevState => ({
+        cursor: prevState.cursor - 1
+      }))
+      this.setState({gray: this.state.gray})
+    } else if (e.keyCode === 40 && cursor < searchResults.length) {
+      this.setState( prevState => ({
+        cursor: prevState.cursor + 1
+      }))
+      console.log(searchResults[cursor])
+    }
+  }
+
+  changeColor(){
+    this.setState({gray: !this.state.gray})
+  }
+
   render(){
+    let itemClass = this.state.gray ? 'unfocused-item' : 'focused-item';
     return(
       <div className="search-engine-container">
-      <h1 className="engine-title">React Search Issues</h1>
-      <input type="text" className="search-box" onChange={this.handleChange} value={this.state.inputValue}></input>
-       <ul> 
-       {
-          (this.state.searchResults || []).map(result => (
-            <li>
-              <div key={result} className="search-results">
-                <div className="text-container">{result}</div>
+        <h1 className="engine-title">React Search Issues</h1>
+        <input 
+          type="text" 
+          className="search-box" 
+          onChange={this.handleChange} 
+          onKeyDown={this.handleKeyDown}
+          value={this.state.inputValue}>
+        </input>
+        <ul> 
+        {
+          (this.state.searchResults || []).map((result, index) => (
+            <li 
+              key={index} 
+              style={ this.state.cursor == index ? { backgroundColor: this.state.gray } : { backgroundColor: !this.state.gray }}>
+              <div className="search-results">
+                <div >{result}</div>
               </div>
             </li>
-            ))
+          ))
         } 
         </ul>
       </div>
--- src/getGitInfo.js
+++ src/getGitInfo.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 function getGithubInfo(itemString, pageCounter){  
   const gitJson = axios.default.get(`https://api.github.com/repos/facebook/react/issues?page=${pageCounter}&per_page=100`, {
     'headers':{
-      'Authorization': `token ${'c2cc47d4780316d574ef2ccfbf0aa86ef33427de'}`
+      'Authorization': `token ${'8618f703d80a1d300125e6591e8c6dc7ed267967'}`
     }
   })
   .then(function(response) {
@@ -26,18 +26,16 @@ export async function GetData(search){
   let stringList = [];
   var pageCounter = 1;  
   let objectData = await getGithubInfo(stringList, pageCounter);      
-  //console.log(objectData);
-  var results = [], result;
+  var results = [];
   if(search === '')
     return results = [''];
   var rx = new RegExp('([^"]*'+search+'[^"]*)','gi');
   for(var key of objectData){
-    while(result = rx.exec(key))
+    if(String(key).match(rx))
       {
         if (results.length >= 5)
           break;
-        if (result !== undefined && result != null && result !== false)
-          results.push(key);          
+        results.push(key);          
       }
   }
   return results;  
